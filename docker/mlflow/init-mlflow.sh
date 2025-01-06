05d883d24e4342b9d59167aaa328a66c952ebd4c
Nils Uhrberg <nils.uhrberg@codecentric.de>
2025-01-06 18:57:52 +0100

    Adjust mlflow entrypoint

--- docker/mlflow/init-mlflow.sh
+++ docker/mlflow/init-mlflow.sh
@@ -2,7 +2,6 @@
 
 # Default values for local storage
 DEFAULT_BACKEND_STORE_URI="sqlite:///mlflow.db"
-DEFAULT_ARTIFACT_ROOT="./mlruns"
 MLFLOW_PORT=5001
 
 # Check for the presence of environment variables and set them if provided
@@ -14,9 +13,8 @@ fi
 
 if [ -n "$MLFLOW_ARTIFACT_ROOT" ]; then
   ARTIFACT_ROOT=$MLFLOW_ARTIFACT_ROOT
+  mlflow server --backend-store-uri $BACKEND_STORE_URI --default-artifact-root $ARTIFACT_ROOT --host 0.0.0.0 --port $MLFLOW_PORT
 else
-  ARTIFACT_ROOT=$DEFAULT_ARTIFACT_ROOT
+  mlflow server --backend-store-uri $BACKEND_STORE_URI --host 0.0.0.0 --port $MLFLOW_PORT
 fi
 
-# Run the MLflow server with the configured backend store URI and artifact root
-mlflow server --backend-store-uri $BACKEND_STORE_URI --default-artifact-root $ARTIFACT_ROOT --host 0.0.0.0 --port $MLFLOW_PORT
